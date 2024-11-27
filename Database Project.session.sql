26af720a0fed43004a8a70d888daba3aa89569bf
Rohit <rohit.truesdale@gmail.com>
2024-11-27 16:58:09 -0600

    removed a sql tools test file

--- Database Project.session.sql
+++
@@ -1,6 +0,0 @@
-SELECT j.JobID
-FROM JobPosition j
-JOIN Department d ON j.DepartmentID = d.Department_ID
-WHERE d.DepartmentName = 'Marketing'
-  AND j.PostedDate >= '2011-01-01'
-  AND j.PostedDate < '2011-02-01'; 
